48140bfaa87a2c3fe8450f122051f692a41c5f2f
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-08 20:21:26 -0300

    Anotando interface com @Repository

--- src/main/java/com/rochards/personapi/repositories/PersonRepository.java
+++ src/main/java/com/rochards/personapi/repositories/PersonRepository.java
@@ -2,6 +2,8 @@ package com.rochards.personapi.repositories;
 
 import com.rochards.personapi.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
